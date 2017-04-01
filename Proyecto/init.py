a18461bf793a342818b1d437faef87affa6bd595
Matias Anoniz <matiasanoniz@gmail.com>
2017-04-01 15:41:58 -0300

    Cambios

--- Proyecto/init.py
+++ Proyecto/init.py
@@ -1,16 +1,17 @@
 MENSAJE_DE_BIENVENIDA = '''
-############################################
+###############################################################
 
-            SISTEMA DE PELICULAS
+                      SISTEMA DE PELICULAS
 
-############################################
+###############################################################
 '''
 
-MAIN_MENU_OPTIONS = [ " Ver peliculas", " Agregar peliculas", " Salir"]
-VER_PELICULAS = [ " Pelicula 1", " Pelicula 2", " Pelicula 3"]
+MAIN_MENU_OPTIONS = [" Ver peliculas", " Agregar peliculas", " Salir"]
+VER_PELICULAS = [" Pelicula 1", " Pelicula 2", " Pelicula 3"]
+
 
 def saludar():
-    print(MENSAJE_DE_BIENVENIDA)
+    print (MENSAJE_DE_BIENVENIDA)
 
 def mostrar_menu_en_pantalla(opciones):
     index = 1
@@ -18,39 +19,33 @@ def mostrar_menu_en_pantalla(opciones):
         print (str(index) + opcion + " >")
         index = index +1
 
-def validar_opcion(indice_opcion, opciones):
-    return indice_opcion <= len(opciones) and indice_opcion > 0
+def validar_opcion(opcion_elegida, opciones_en_lista):
+    # Devuelve cantidad de opciones que hay en la lista de MAIN_MENU_OPTIONS
+    return opcion_elegida <= len(opciones_en_lista) and opcion_elegida > 0
 
-def mostrar_menu(opciones, funcion):
+def mostrar_menu(opciones, funciones):
     mostrar_menu_en_pantalla(opciones)
-    inidice_opcion = obtener_input_usuarios()
-    es_valido = validar_opcion(indice_opcion, opciones)
+    numero_de_opcion = obtener_input_usuarios()
+    es_valido = validar_opcion(numero_de_opcion, opciones)
     if es_valido:
-        funcionEsValida(indice_opcion)
+        funcionEsValida(numero_de_opcion)
     else:
-        mostrar_menu(opciones, funcionvalida)
+        print("Opcion Incorrecta, ingrese una opcion")
+        mostrar_menu(opciones, numero_de_opcion)
 
-def funcionEsVAlida (opcion_elegida):
+def funcionEsValida (opcion_elegida):
     if (opcion_elegida == 1):
-        print ("Elegiste Opcion :", opcion_elegida)
-    elif (opcion_elegida == 3):
-        exit()
-
-def valores (opcion_elegida):
-    if (opcion_elegida == 1):
-        mostrar_menu(VER_PELICULAS, verPelicula)
+        print ("Elegiste en esta opcion, y elegiste :", opcion_elegida)
     elif (opcion_elegida == 2):
-        print ("Agregar Pelicula")
+        print ("Elegiste la Opcion :", opcion_elegida)
     elif (opcion_elegida == 3):
         exit()
 
 def obtener_input_usuarios():
     # print(chr(27)+"[1;33m"+"Ingrese opcion deseada : ")
     indice_opcion = int(raw_input(chr(27)+"[1;33m"+"Ingrese opcion deseada : "))
-    print(chr(27)+"[0m")
     return indice_opcion
 
 if __name__ == '__main__':
     saludar()
-    mostrar_menu(MAIN_MENU_OPTIONS, funcion)
-    obtener_input_usuarios()
+    mostrar_menu(MAIN_MENU_OPTIONS, funcionEsValida)
