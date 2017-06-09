e828aa2cdf52c29cc54a06d962f0ce62a6a48d69
Matias Anoniz <matiasanoniz@gmail.com>
2017-06-09 12:07:56 -0300

    Correo con python

---
+++ mail/setup.py
@@ -0,0 +1,7 @@
+# http://www.taringa.net/posts/hazlo-tu-mismo/17698845/Compilar-tu-programa-en-Python-en-un-exe-unico.html
+
+from distutils.core import setup 
+import py2exe 
+import os 
+
+setup(console=["correo.py"]) 
