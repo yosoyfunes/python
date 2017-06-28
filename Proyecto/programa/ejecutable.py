c591d05c8e92987552cfc6215e5a4e3ec4c23386
Matias Anoniz <matiasanoniz@gmail.com>
2017-06-28 12:34:51 -0300

    Proyecto

--- Proyecto/programa/ejecutable.py
+++ Proyecto/programa/ejecutable.py
@@ -16,12 +16,40 @@ base = None
 if sys.platform == "win32":
     base = "Win32GUI"
 
-setup(  name = "ventana",
-        version = "0.1" ,
-        description = "ventana" ,
+company_name = 'My Company Name'
+product_name = 'My Gui'
+
+bdist_msi_options = {
+    'upgrade_code': '{66620F3A-DC3A-11E2-B341-002219E9B01E}',
+    'add_to_path': False,
+    'initial_target_dir': r'[ProgramFilesFolder]\%s\%s' % (company_name, product_name),
+    # 'includes': ['atexit', 'PySide.QtNetwork'], # <-- this causes error
+    }
+
+target = Executable(
+    script="main.pyw",
+    base= base,
+#    compress=False,
+#    copyDependentFiles=True,
+#    appendScriptToExe=True,
+#    appendScriptToLibrary=False,
+    icon='farmacia.ico',
+    shortcutName="Sistema de Turnos",
+    shortcutDir="DesktopFolder"
+    )
+
+setup(  name = "PyPet",
+        version = "1.0.1",
+        description = "Sistema de Veterinaria" ,
+        author="Matias Anoniz",
         # options = {"build_exe": build_exe_options},
-        options = {'build_exe': {'includes':includes, 'excludes':excludes, 'packages':packages, 'include_files':includefiles}}, 
-        executables = [Executable("main.pyw", base=base)])
+        options = {
+                'build_exe': {'includes':includes, 'excludes':excludes, 'packages':packages, 'include_files':includefiles},
+                'build_msi': {'includes':includes, 'excludes':excludes, 'packages':packages, 'include_files':includefiles}
+        }, 
+        # executables = [Executable("main.pyw", base=base)])
+        executables=[target]
+     )
 
 '''  1. Este archivo le he llamado ejecutable.py. Llámale como quieras
      2. Sustituye en Executable el nombre del archivo py o pyw  por el que quieres convertir a exe.
@@ -34,4 +62,26 @@ setup(  name = "ventana",
     NOTA: Recuerda que debes tener instalado cx_freeze. 
     Lo puedes hacer desde la línea de comandos con:  pip install cx_Freeze
 
-    '''
+'''
+
+'''
+target = Executable(
+    script="your_program.py",
+    base="Win32GUI",
+    compress=False,
+    copyDependentFiles=True,
+    appendScriptToExe=True,
+    appendScriptToLibrary=False,
+    icon="icon.ico"
+    )
+
+setup(
+    name="name",
+    version="1.0",
+    description="the description",
+    author="the author",
+    options={"build_exe": options},
+    executables=[target]
+    )
+
+'''
